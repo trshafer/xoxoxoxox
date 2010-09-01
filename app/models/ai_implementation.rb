11e1f9ccf1bf8954a3e9339b6657307b4629802d
Thomas Shafer <thomasjshafer@gmail.com>
2010-08-31 18:33:35 -0700

    restored comments in default ai_implementation code

--- app/models/ai_implementation.rb
+++ app/models/ai_implementation.rb
@@ -8,15 +8,21 @@ class AiImplementation < ActiveRecord::Base
   validates_presence_of :name
 
   protected
-  # // Create your own AI
-  # // the only function you must implement is a move function
-  # // to get user spaces call: Board.spaceIdsFor('user');
-  # // to get computer spaces call: Board.spaceIdsFor('computer');
-  # // to get empty spaces call: Board.emptySpaceIds();
-  # // To see if you have a winning set use: Rules.playerWins(playerIdsArray);
-  # // if you get frustrated, you can alwasys go back to Board.emptySpaceIds().random();
   def set_default_code
-    self.code = "//#{self.name}\nfunction move(){\n  return Board.emptySpaceIds().random();\n}"
+self.code = <<-eos
+// #{self.name}
+// Create your own AI
+// the only function you must implement is a move function
+// to get user spaces call: Board.spaceIdsFor('user');
+// to get computer spaces call: Board.spaceIdsFor('computer');
+// to get empty spaces call: Board.emptySpaceIds();
+// To see if you have a winning set use: Rules.playerWins(playerIdsArray);
+// if you get frustrated, you can alwasys go back to Board.emptySpaceIds().random();
+function move(){
+  Logger.info('Making move from #{self.name}')
+  return Board.emptySpaceIds().random();
+}
+eos
   end
 
 end
