bb40c483c0ea35c98bd8db364b3f22f63d710aeb
Thomas Shafer <thomasjshafer@gmail.com>
2010-09-01 20:56:08 -0700

    better login/signup page. Error messages still overflow :/

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
   before_create :generate_first_code
 
   acts_as_authentic do |c|
-    c.validate_email_field = false
+    c.merge_validates_format_of_email_field_options(:allow_blank => true) 
+    c.merge_validates_length_of_email_field_options(:allow_blank => true) 
+    c.merge_validates_uniqueness_of_email_field_options(:allow_blank => true)
   end
   
   protected
