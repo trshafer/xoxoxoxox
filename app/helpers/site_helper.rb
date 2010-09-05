3e55dc976bd20164cbb7e758d3996ce2c0220d34
Thomas Shafer <thomasjshafer@gmail.com>
2010-09-05 12:22:56 -0700

    added some form of tech specs

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,7 +1,11 @@
 module SiteHelper
   
-  def link_to_void(name, options={})
-    link_to name, 'javascript:void(0)', options
+  def link_to_void(name, options={}, html_options=nil)
+    link_to name, 'javascript:void(0)', options, html_options
+  end
+  
+  def link_to_external(name, url=nil, options={}, html_options=nil)
+    link_to name, url || name, options.merge(:target => '_blank'), html_options
   end
   
 end
