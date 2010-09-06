94b60508bbaf84016a336a7bddaeb1d248858364
Thomas Shafer <thomasjshafer@gmail.com>
2010-09-06 10:52:45 -0700

    recorded final screencast. Let's get this out therrrr!
    Signed-off-by: Thomas Shafer <thomasjshafer@gmail.com>

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,6 @@
 module DashboardsHelper
+  
+  def embedded_screencast
+    '<object id="scPlayer" class="embeddedObject" width="100%" height="100%" type="application/x-shockwave-flash" data="http://content.screencast.com/users/thomasshafer/folders/Jing/media/77cb7cbb-5cb7-454d-878f-86840498e480/jingswfplayer.swf" >  <param name="movie" value="http://content.screencast.com/users/thomasshafer/folders/Jing/media/77cb7cbb-5cb7-454d-878f-86840498e480/jingswfplayer.swf" />  <param name="quality" value="high" />  <param name="bgcolor" value="#FFFFFF" />  <param name="flashVars" value="thumb=http://content.screencast.com/users/thomasshafer/folders/Jing/media/77cb7cbb-5cb7-454d-878f-86840498e480/FirstFrame.jpg&containerwidth=1023&containerheight=695&content=http://content.screencast.com/users/thomasshafer/folders/Jing/media/77cb7cbb-5cb7-454d-878f-86840498e480/00000003.swf&blurover=false" />  <param name="allowFullScreen" value="true" />  <param name="scale" value="showall" />  <param name="allowScriptAccess" value="always" />  <param name="base" value="http://content.screencast.com/users/thomasshafer/folders/Jing/media/77cb7cbb-5cb7-454d-878f-86840498e480/" /> </object>'
+  end
 end
